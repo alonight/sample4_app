6f0d89ec152277a275e89bd3ad082088301050bd
alonight <alonight@live.cn>
2013-12-06 16:19:40 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smaple4App::Application.config.secret_key_base = '2b22bf054fbf59f976304f732abb3d97778f3df1121f5ec3c689d8e236014d13994f2ab52ec55e3a4c3e3135ceb48a40bc9d0b3b1c075ee40b75d26b6274ff75'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Smaple4App::Application.config.session_store :cookie_store, key: '_smaple4_app_session'
